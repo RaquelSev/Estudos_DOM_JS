9f9e7185d8f638db92e1eab678acf7b9e0ac3a98
Raquel Severo <raquel.sev.celia@gmail.com>
2021-10-05 20:01:48 -0300

    Removendo elemento

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -18,7 +18,9 @@
         tarefa.innerHTML = conteudo;
 
         tarefa.appendChild(BotaoConclui());
+        tarefa.appendChild(BotaoDeleta());
         lista.appendChild(tarefa);
+        
 
         input.value = " ";
     }
@@ -47,6 +49,24 @@
         tarefaCompleta.classList.toggle('done');
 
     }
-}
 
-)();
+    const BotaoDeleta = () => {
+        const botaoDeleta = document.createElement('button');
+
+        botaoDeleta.innerText = 'deletar';
+        botaoDeleta.addEventListener('click', deletarTarefa);
+
+        return botaoDeleta;
+
+    }
+
+    const deletarTarefa = (evento) => {
+        const botaoDeleta = evento.target;
+
+        const tarefaCompleta = botaoDeleta.parentElement;
+
+        tarefaCompleta.remove();
+        
+        return botaoDeleta;
+    }
+})();
